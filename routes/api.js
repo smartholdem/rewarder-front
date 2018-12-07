82c4f4ba02cc4f4acb1673d1a0dcd06d96927b8d
technolog <viktor@allz.su>
2018-12-08 02:54:36 +0300

    delegate init into service

--- routes/api.js
+++ routes/api.js
@@ -1,31 +1,80 @@
 var express = require('express');
 var router = express.Router();
+const jsonReader = require('jsonfile');
+const level = require("level");
+const request = require("request");
+const db = level('.db', {valueEncoding: 'json'});
+const cryptoRandomString = require('crypto-random-string');
 
+// 0x - reserved
+// 1x - list delegates
+// 2x - delegate stat
+// 3x - bad delegates
+// 4x - service stats
 
 /* API Functions */
-
 function initApp() {
 
 }
 
+// db.del('1xgendelegate_2');
+
 /* API */
-router.post('/registerDelegate', function(req, res, next) {
+router.post('/init', function (req, res, next) {
+    let delegateOptions = req.body;
+    console.log(delegateOptions);
+    db.get('1x' + delegateOptions.name, function (err, value) {
+        if (err) {
+            let key = cryptoRandomString(12);
+            db.put('1x' + delegateOptions.name, {
+                "delegate": delegateOptions.delegate,
+                "paymentPeriod": delegateOptions.paymentPeriod,
+                "paymentPercent": delegateOptions.paymentPercent,
+                "minVoteWeight": delegateOptions.minVoteWeight,
+                "timereg": Date.now(),
+                "verified": false,
+                "private": {
+                    "key": key
+                }
+            }, function (errPut) {
+                if (!errPut) {
+                    res.json({"init": true, "key": key});
+                }
+            });
+        } else {
+            res.json({"init": false});
+        }
+    });
+});
+
+router.post('/verify', function (req, res, next) {
     res.json(true);
 });
 
-router.post('/verifyDelegate', function(req, res, next) {
+router.get('/delegates', function (req, res, next) {
+    let list = [];
+    db.createReadStream({gte: '1x', lt: '2x', "limit": 1000})
+        .on('data', function (data) {
+            list.push(data.value);
+            // list[data.key] = data.value;
+        })
+        .on('error', function (err) {
+            console.log('Oh my!', err)
+        })
+        .on('close', function () {
+            // console.log('Stream closed')
+        })
+        .on('end', function () {
+            // console.log('Stream ended');
+            res.json(list);
+        });
+});
+
+router.get('/delegate/detail', function (req, res, next) {
     res.json(true);
 });
 
-router.get('/getListDelegates', function(req, res, next) {
-    res.json(true);
-});
-
-router.get('/detailDelegateInfo', function(req, res, next) {
-    res.json(true);
-});
-
-router.get('/getDelegateStats', function(req, res, next) {
+router.get('/delegate/stats', function (req, res, next) {
     res.json(true);
 });
 
